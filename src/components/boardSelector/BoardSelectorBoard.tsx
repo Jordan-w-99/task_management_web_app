3ad0a95b823bf27a543a5cd6e060729808cafb65
Jordan Wright <jordanw99@hotmail.co.uk>
2024-02-18 22:47:49 +0000

    sticky note interactivity

--- src/components/boardSelector/BoardSelectorBoard.tsx
+++ src/components/boardSelector/BoardSelectorBoard.tsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
 import { BoardDetails } from '../../models/boardDetails'
 import styles from './BoardSelectorBoard.module.css'
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
 
 export interface BoardSelectorBoardProps {
     details: BoardDetails
@@ -18,9 +18,20 @@ export const BoardSelectorBoard = ({ details }: BoardSelectorBoardProps) => {
     }, 0) % 20) - 10
         , [details.id]);
 
+    const [hovered, setHovered] = useState(false)
+
     return (
         <Link to={`board/${details.id}`}>
-            <div className={styles.container} style={{ rotate: `${rotationFromId}deg` }}>
+            <div
+                className={styles.container}
+                style={{
+                    rotate: `${hovered ? 0 : rotationFromId}deg`,
+                    scale: `${hovered ? 1.05 : 1}`,
+                    boxShadow: `${hovered ? '5px 5px 8px rgba(33, 33, 33, .6)' : '5px 5px 7px rgba(33, 33, 33, .7)'}`,
+                }}
+                onMouseEnter={() => setHovered(true)}
+                onMouseLeave={() => setHovered(false)}
+            >
                 <div className={styles.noteTop} />
                 <div className={styles.noteBottom}>
                     <p className={styles.boardName}>{details.title}</p>
